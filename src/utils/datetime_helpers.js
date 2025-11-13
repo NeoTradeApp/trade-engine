2e3c75ca065d8e37bc052d6aca9ef2a0352bc623
Ibrahim Saifee <ibrahim.saifee786@gmail.com>
2025-11-13 19:43:47 +0530

    Fixed the app name and helper methods.

--- src/utils/datetime_helpers.js
+++ src/utils/datetime_helpers.js
@@ -8,14 +8,14 @@ const todayTimeIst = (time) => {
   return now;
 };
 
-const marketOpeningTime = () => todayTimeIst({ hour: 9, minute: 14 });
+const marketOpeningTime = todayTimeIst({ hour: 9, minute: 14 });
 
-const marketClosingTIme = () => todayTimeIst({ hour: 15, minute: 31 });
+const marketClosingTIme = todayTimeIst({ hour: 15, minute: 31 });
 
 const isMarketOpen = () => {
   const now = todayTimeIst();
 
-  return now.isBetween(marketOpeningTime(), marketClosingTIme());
+  return now.isBetween(marketOpeningTime, marketClosingTIme);
 };
 
 module.exports = {
